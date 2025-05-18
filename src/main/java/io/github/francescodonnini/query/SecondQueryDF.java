2ed5fe60553641e784f528e3bc7f80e21002259e
Francesco Donnini <donnini.francesco00@gmail.com>
2025-05-18 19:18:14 +0200

    Cominciato ad agg InfluxDB

--- src/main/java/io/github/francescodonnini/query/SecondQueryDF.java
+++ src/main/java/io/github/francescodonnini/query/SecondQueryDF.java
@@ -1,19 +1,28 @@
 package io.github.francescodonnini.query;
 
+import com.influxdb.client.InfluxDBClient;
+import com.influxdb.client.write.Point;
 import io.github.francescodonnini.dataset.CsvField;
 import org.apache.spark.sql.Column;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
 import static org.apache.spark.sql.functions.*;
 
 public class SecondQueryDF implements Query {
+    private static final String YEAR_MONTH_COL = "yearMonth";
+    private static final String AVG_CARBON_INTENSITY_COL = "avgCi";
+    private static final String AVG_CFE_PERCENTAGE_COL = "avgCfe";
     private final SparkSession spark;
     private final String datasetPath;
+    private final InfluxDBClient influxDBClient;
     private final String resultsPath;
 
-    public SecondQueryDF(SparkSession spark, String datasetPath, String resultsPath) {
+    public SecondQueryDF(SparkSession spark, String datasetPath, InfluxDBClient influxDBClient, String resultsPath) {
         this.spark = spark;
         this.datasetPath = datasetPath;
+        this.influxDBClient = influxDBClient;
         this.resultsPath = resultsPath;
     }
 
@@ -24,32 +33,28 @@ public class SecondQueryDF implements Query {
 
     @Override
     public void submit() {
-        final var yearMonthCol = "yearMonth";
-        final var avgCarbonIntensityCol = "avgCi";
-        final var avgCfePercentageCol = "avgCfe";
         var averages = spark.read().parquet(datasetPath + ".parquet")
-                .withColumn(yearMonthCol, getYearMonth(CsvField.DATETIME_UTC.getName()))
-                .select(
-                        col(yearMonthCol),
+                .withColumn(YEAR_MONTH_COL, getYearMonth(CsvField.DATETIME_UTC.getName()))
+                .select(col(YEAR_MONTH_COL),
                         col(CsvField.COUNTRY.getName()),
                         col(CsvField.CARBON_INTENSITY_DIRECT.getName()),
                         col(CsvField.CFE_PERCENTAGE.getName()))
                 .where(col(CsvField.COUNTRY.getName()).equalTo("Italy"))
-                .groupBy(col(yearMonthCol))
-                .agg(avg(CsvField.CARBON_INTENSITY_DIRECT.getName()).as(avgCarbonIntensityCol),
-                     avg(CsvField.CFE_PERCENTAGE.getName()).as(avgCfePercentageCol))
-                .orderBy(col(yearMonthCol));
+                .groupBy(col(YEAR_MONTH_COL))
+                .agg(avg(CsvField.CARBON_INTENSITY_DIRECT.getName()).as(AVG_CARBON_INTENSITY_COL),
+                     avg(CsvField.CFE_PERCENTAGE.getName()).as(AVG_CFE_PERCENTAGE_COL))
+                .orderBy(col(YEAR_MONTH_COL));
         var ciDesc = averages
-                .orderBy(col(avgCarbonIntensityCol).desc())
+                .orderBy(col(AVG_CARBON_INTENSITY_COL).desc())
                 .limit(5);
         var ciAsc = averages
-                .orderBy(col(avgCarbonIntensityCol).asc())
+                .orderBy(col(AVG_CARBON_INTENSITY_COL).asc())
                 .limit(5);
         var cfeDesc = averages
-                .orderBy(col(avgCfePercentageCol).desc())
+                .orderBy(col(AVG_CFE_PERCENTAGE_COL).desc())
                 .limit(5);
         var cfeAsc = averages
-                .orderBy(col(avgCfePercentageCol).asc())
+                .orderBy(col(AVG_CFE_PERCENTAGE_COL).asc())
                 .limit(5);
         averages.write()
                 .option("header", true)
@@ -64,5 +69,17 @@ public class SecondQueryDF implements Query {
 
     private Column getYearMonth(String colName) {
         return date_format(to_timestamp(col(colName), CsvField.getDateTimeFormat()), "yyyy-MM");
+    }
+
+    private void saveToInfluxDB(Dataset<Row> dataset) {
+        var writer = influxDBClient.getWriteApiBlocking();
+        dataset.foreachPartition(rows -> {
+            rows.forEachRemaining(row -> {
+                var point = Point.measurement("ci")
+                        .addTag("country", row.getAs(CsvField.COUNTRY.getName()))
+                        .addTag("time", row.getAs(YEAR_MONTH_COL))
+                        .addField("value", row.getDouble(AVG_CARBON_INTENSITY_COL));
+            });
+        });
     }
 }
--- src/main/java/io/github/francescodonnini/query/SecondQueryRDD.java
+++ src/main/java/io/github/francescodonnini/query/SecondQueryRDD.java
@@ -1,11 +1,14 @@
 package io.github.francescodonnini.query;
 
 import io.github.francescodonnini.dataset.CsvField;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import scala.Tuple2;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 
 public class SecondQueryRDD implements Query {
     private final SparkSession spark;
@@ -13,7 +16,10 @@ public class SecondQueryRDD implements Query {
     private final String resultsPath;
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern(CsvField.getDateTimeFormat());
 
-    public SecondQueryRDD(SparkSession spark, String datasetPath, String resultsPath) {
+    public SecondQueryRDD(
+            SparkSession spark,
+            String datasetPath,
+            String resultsPath) {
         this.spark = spark;
         this.datasetPath = datasetPath;
         this.resultsPath = resultsPath;
@@ -26,37 +32,49 @@ public class SecondQueryRDD implements Query {
 
     @Override
     public void submit() {
-        spark.sparkContext()
-            .textFile(datasetPath, 1)
-            .toJavaRDD()
-            .filter(line -> countryFilter(line, "Italy"))
-            .mapToPair(this::toPair)
-            .reduceByKey(QueryUtils::sumDoubleIntPair)
-            .map(QueryUtils::average)
-            .map(this::stringify)
-            .saveAsTextFile(resultsPath);
+        var averages = spark.read()
+                .parquet(datasetPath + ".parquet")
+                .filter(this::italianZone)
+                .javaRDD()
+                .mapToPair(this::toPair)
+                .reduceByKey(QueryUtils::sumDoubleIntPair)
+                .mapToPair(QueryUtils::average)
+                .sortByKey();
+
+        var tops = new ArrayList<Tuple2<Tuple2<Integer, Integer>, Tuple2<Double, Double>>>();
+        tops.addAll(averages.takeOrdered(5, (x, y) -> ciCompareTo(x, y, false)));
+        tops.addAll(averages.takeOrdered(5, (x, y) -> ciCompareTo(x, y, true)));
+        tops.addAll(averages.takeOrdered(5, (x, y) -> cfeCompareTo(x, y, false)));
+        tops.addAll(averages.takeOrdered(5, (x, y) -> cfeCompareTo(x, y, true)));
     }
 
-    private String stringify(Tuple2<Tuple2<Integer, Integer>, Tuple2<Double, Double>> x) {
+    private boolean italianZone(Row r) {
+        return r.getString(CsvField.ZONE_ID.getIndex()).equals("IT");
+    }
+
+    private void saveToCache(JavaPairRDD<Tuple2<Integer, Integer>, Tuple2<Double, Double>> plot) {
+        throw new UnsupportedOperationException("not yet implemented");
+    }
+
+    private int cfeCompareTo(Tuple2<Tuple2<Integer, Integer>, Tuple2<Double, Double>> x, Tuple2<Tuple2<Integer, Integer>, Tuple2<Double, Double>> y, boolean asc) {
+        return asc ? x._2()._2().compareTo(y._2()._2()) : y._2()._2().compareTo(x._2()._2());
+    }
+
+    private int ciCompareTo(Tuple2<Tuple2<Integer, Integer>, Tuple2<Double, Double>> x, Tuple2<Tuple2<Integer, Integer>, Tuple2<Double, Double>> y, boolean asc) {
+        return asc ? x._2()._1().compareTo(y._2()._1()) : y._2()._1().compareTo(x._2()._1());
+    }
+
+    private String toCsv(Tuple2<Tuple2<Integer, Integer>, Tuple2<Double, Double>> x) {
         return x._1()._1() + "," + x._1()._2() + "," + x._2()._1() + "," + x._2()._2();
     }
 
-    private Tuple2<Tuple2<Integer, Integer>, Tuple2<Tuple2<Double, Integer>, Tuple2<Double, Integer>>> toPair(String line) {
-        var fields = getFields(line);
-        var date = LocalDateTime.parse(fields[CsvField.DATETIME_UTC.getIndex()], formatter);
+    private Tuple2<Tuple2<Integer, Integer>, Tuple2<Tuple2<Double, Integer>, Tuple2<Double, Integer>>> toPair(Row line) {
+        var date = LocalDateTime.parse(line.getString(CsvField.DATETIME_UTC.getIndex()), formatter);
         return new Tuple2<>(
                 new Tuple2<>(date.getYear(), date.getMonth().getValue()),
                 new Tuple2<>(
-                        new Tuple2<>(Double.parseDouble(fields[CsvField.CARBON_INTENSITY_DIRECT.getIndex()]), 1),
-                        new Tuple2<>(Double.parseDouble(fields[CsvField.CFE_PERCENTAGE.getIndex()]), 1)));
+                        new Tuple2<>(line.getDouble(CsvField.CARBON_INTENSITY_DIRECT.getIndex()), 1),
+                        new Tuple2<>(line.getDouble(CsvField.CFE_PERCENTAGE.getIndex()), 1)));
     }
 
-    private boolean countryFilter(String line, String countryCode) {
-        var fields = getFields(line);
-        return fields[CsvField.COUNTRY.getIndex()].equals(countryCode);
-    }
-
-    private String[] getFields(String line) {
-        return line.split(",");
-    }
 }
