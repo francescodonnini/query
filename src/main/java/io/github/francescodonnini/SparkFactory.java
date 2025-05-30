c3872bff2b7f20a5c218e35a22147b155909f11a
Francesco Donnini <donnini.francesco00@gmail.com>
2025-05-30 11:52:57 +0200

    Impostato livello del logger a WARN per diminuire il numero di messaggi in stdout

--- src/main/java/io/github/francescodonnini/SparkFactory.java
+++ src/main/java/io/github/francescodonnini/SparkFactory.java
@@ -7,9 +7,11 @@ public class SparkFactory {
     private SparkFactory() {}
 
     public static SparkSession getSparkSession(Conf conf) {
-        return SparkSession.builder()
+        var spark = SparkSession.builder()
                 .master(String.format("spark://%s:%d", conf.getString("SPARK_MASTER_HOST"), conf.getInt("SPARK_MASTER_PORT")))
                 .appName(conf.getString("SPARK_APP_NAME"))
                 .getOrCreate();
+        spark.sparkContext().setLogLevel("WARN");
+        return spark;
     }
 }
