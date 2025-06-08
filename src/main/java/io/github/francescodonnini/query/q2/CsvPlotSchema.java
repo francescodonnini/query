7bab501dbf6f43c31ad5f2a8177dff6f2937bb99
Francesco Donnini <donnini.francesco00@gmail.com>
2025-06-08 15:20:21 +0200

    Spostati campi static in cima alla classe

--- src/main/java/io/github/francescodonnini/query/q2/CsvPlotSchema.java
+++ src/main/java/io/github/francescodonnini/query/q2/CsvPlotSchema.java
@@ -1,9 +1,9 @@
 package io.github.francescodonnini.query.q2;
 
 public class CsvPlotSchema {
-    private CsvPlotSchema() {}
-    
     public static final String YEAR_MONTH = "yearMonth";
     public static final String AVG_CARBON_INTENSITY_DIRECT = "avg_carbon_intensity_gco2eq_per_kwh_direct)";
     public static final String AVG_CARBON_FREE_ENERGY_PERCENTAGE = "avg_carbon_free_energy_percentage_cfe)";
+
+    private CsvPlotSchema() {}
 }
