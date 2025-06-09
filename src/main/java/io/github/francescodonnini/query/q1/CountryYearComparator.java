4c75e20df56d6fbcce8d987e89afc3b0bdc719e6
Francesco Donnini <donnini.francesco00@gmail.com>
2025-06-09 21:03:33 +0200

    corretto comparator per coppia Paese, Anno: il confronto per anno va fatto quando i Paesi sono uguali

--- src/main/java/io/github/francescodonnini/query/q1/CountryYearComparator.java
+++ src/main/java/io/github/francescodonnini/query/q1/CountryYearComparator.java
@@ -9,7 +9,7 @@ public class CountryYearComparator implements Comparator<Tuple2<String, Integer>
     @Override
     public int compare(Tuple2<String, Integer> o1, Tuple2<String, Integer> o2) {
         var cmp = o1._1().compareTo(o2._1());
-        if (cmp != 0) {
+        if (cmp == 0) {
             return Integer.compare(o1._2(), o2._2());
         }
         return cmp;
